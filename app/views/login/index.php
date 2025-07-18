75989587184c98aa5c7ddca9d5175a43cead0ab5
Muhammad Abdullah Adil <abdullahadil143@gmail.com>
2025-07-18 23:20:38 +0000

    center align login fields

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,51 +1,48 @@
 <?php require_once 'app/views/templates/headerPublic.php' ?>
 <main role="main" class="container">
-		<div class="page-header" id="banner">
-				<div class="row">
-						<div class="col-lg-12">
-								<h1>You are not logged in</h1>
-						</div>
-				</div>
-		</div>
+    <div class="page-header" id="banner">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="text-center">You are not logged in</h1>
+            </div>
+        </div>
+    </div>
 
-		<div class="row">
-				<div class="col-sm-auto">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
 
-						<?php
-						if (isset($_SESSION['loginError'])) {
-								echo "<p style='color:red; font-weight:bold;'>" . $_SESSION['loginError'] . "</p>";
-								unset($_SESSION['loginError']);
-						}
-						if (isset($_SESSION['registerSuccess'])) {
-							echo "<p style='color:green; font-weight:bold;'>" . $_SESSION['registerSuccess'] . "</p>";
-							unset($_SESSION['registerSuccess']);
-					}
+            <?php
+            if (isset($_SESSION['loginError'])) {
+                echo "<p style='color:red; font-weight:bold; text-align:center;'>" . $_SESSION['loginError'] . "</p>";
+                unset($_SESSION['loginError']);
+            }
+            if (isset($_SESSION['registerSuccess'])) {
+                echo "<p style='color:green; font-weight:bold; text-align:center;'>" . $_SESSION['registerSuccess'] . "</p>";
+                unset($_SESSION['registerSuccess']);
+            }
+            ?>
 
-						?>
+            <div class="p-4 rounded bg-white shadow-sm">
+                <form action="/login/verify" method="post">
+                    <fieldset>
+                        <div class="form-group mb-3">
+                            <label for="username">Username</label>
+                            <input required type="text" class="form-control" name="username">
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="password">Password</label>
+                            <input required type="password" class="form-control" name="password">
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <button type="submit" class="btn btn-primary">Login</button>
+                            <a href="/create" class="btn btn-secondary">Register</a>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
 
-						<div style="margin-bottom: 40px;">
-								<form action="/login/verify" method="post">
-										<fieldset>
-												<div class="form-group">
-														<label for="username">Username</label>
-														<input required type="text" class="form-control" name="username">
-												</div>
-												<div class="form-group">
-														<label for="password">Password</label>
-														<input required type="password" class="form-control" name="password">
-												</div>
-												<br>
-											<div class="d-flex gap-2">
-													<button type="submit" class="btn btn-primary">Login</button>
-													<a href="/create" class="btn btn-primary">Register</a>
-											</div>
+            <p class="text-muted text-center mt-4">Copyright, &copy; 2025</p>
 
-										</fieldset>
-								</form>
-						</div>
-
-						<p class="text-muted" style="text-align:center; margin-top: 20px;">Copyright,&copy; 2025</p>
-
-				</div>
-		</div>
+        </div>
+    </div>
 </main>
