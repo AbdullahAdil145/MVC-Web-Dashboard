4c1f595fb2b7b4e8c1aa393305d0e863bae6af9a
Muhammad Abdullah Adil <abdullahadil143@gmail.com>
2025-07-18 23:50:44 +0000

    center align register page

--- app/views/create/index.php
+++ app/views/create/index.php
@@ -1,21 +1,25 @@
 <?php require_once 'app/views/templates/headerPublic.php' ?>
-<main role="main" class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>Register an Account</h1>
-            </div>
-        </div>
-    </div>
 
-    <div class="row">
-        <div class="col-sm-auto">
+<main role="main" class="container">
+
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+
+            <div class="page-header" id="banner">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="text-center">Register</h1>
+                    </div>
+                </div>
+            </div>
+
             <?php
             if (isset($_SESSION['registerError'])) {
-                echo "<p style='color:red; font-weight:bold;'>" . $_SESSION['registerError'] . "</p>";
+                echo "<p style='color:red; font-weight:bold; text-align:center;'>" . $_SESSION['registerError'] . "</p>";
                 unset($_SESSION['registerError']);
             }
             ?>
+
             <div style="margin-bottom: 40px;">
                 <form action="/create/submit" method="post">
                     <fieldset>
@@ -34,7 +38,10 @@
                     </fieldset>
                 </form>
             </div>
-            <p class="text-muted" style="text-align:center; margin-top: 20px;">&copy; 2025</p>
+
+            <p class="text-muted text-center" style="margin-top: 20px;">&copy; 2025</p>
+
         </div>
     </div>
+
 </main>
