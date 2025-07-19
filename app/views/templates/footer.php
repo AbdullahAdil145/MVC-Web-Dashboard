2161dbffc53f92e475f6bbd7982b653acda2a0b0
Muhammad Abdullah Adil <abdullahadil143@gmail.com>
2025-07-19 01:08:59 +0000

    fixed header and footer styling in dark mode

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,8 +1,9 @@
-<footer class="bg-light text-center py-3 mt-5">
+<footer class="custom-footer text-center py-3 mt-5">
     <p class="mb-0">
         © <?= date('Y'); ?> Muhammad Abdullah Adil. All rights reserved.
     </p>
 </footer>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
